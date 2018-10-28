aca29c8cd3855f05c769e3b686dcb43385a806d0
Leonardo Cunha Moreto <leonardo.moreto@MacBook-Air-de-Yaman-3.local>
2018-10-28 01:36:44 -0300

    Criacao de diversos metodos para servicesTodo

--- services/todos.js
+++ services/todos.js
@@ -1,9 +1,29 @@
 const Todos = require('../models/todos');
 
+const all = async () => {
+  return await Todos.find({});
+}
+
+const byId = async (id) => {
+  return await Todos.findOne({ _id: id });
+};
+
 const Create = async (data) => {
   return await Todos.create(data);
 };
 
+const Update = async(id, name) => {
+  return await Todos.updateOne({ _id: id }, { $set: { name } });
+};
+
+const Delete = async(id) => {
+  return await Todos.remove({ _id: id });
+}
+
 module.exports = {
-  Create
+  all,
+  byId,
+  Create,
+  Update,
+  Delete
 }
